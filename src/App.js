780c49a1c4c47d812c3bdf5a2c0d4bdc3f686865
rnin9 <rkdalswn1209@kw.ac.kr>
2020-04-16 22:44:26 +0900

    Add Genre

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Axios from 'axios';
 import Movie from './Movie';
+import "./App.css";
 
 class App extends React.Component{
   state = {
@@ -10,27 +11,33 @@ class App extends React.Component{
   getMovies = async() => { 
     const {data:{data:{movies}}}=  await Axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
     this.setState({movies , isLoading: false});
-    console.log({movies});
   }
   componentDidMount(){
     this.getMovies();
   }
   render(){
     const {isLoading, movies} = this.state;
-    return (<div>{ isLoading ? "Loading...." : movies.map(movie =>{
-      console.log(movie);
-      return(
-        <Movie
-              key ={movie.id} 
-              id={movie.id} 
-              year={movie.year} 
-              title={movie.title} 
-              summary={movie.summary} 
-              poster={movie.medium_cover_image}
-              />
-            );
-  })}
-  </div>);
+    return (
+    <section className ="container">
+      { isLoading ? (
+          <div className="loader">
+            <span className="loader__text">"Loading...."</span>
+          </div>) : ( 
+                  <div className="movies">
+                  {movies.map(movie =>(
+                    <Movie
+                      key ={movie.id} 
+                      id={movie.id} 
+                      year={movie.year} 
+                      title={movie.title} 
+                      summary={movie.summary} 
+                      poster={movie.medium_cover_image}
+                      genres={movie.genres}
+                    />
+                  ))}  
+                  </div>
+      )}
+  </section>);
   }
 }
 
